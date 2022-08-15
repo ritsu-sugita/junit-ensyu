362b93c9d34ffd28c8f8f82c4ccea72c079c5d17
sugitaritsu <ritsu.sugita@rakus-partners.co.jp>
2022-08-15 12:10:45 +0900

    commit1

---
+++ src/test/java/junit/tutorial/ex01/e01/StringUtilsTest.java
@@ -0,0 +1,52 @@
+package junit.tutorial.ex01.e01;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.example.StringUtils;
+
+class StringUtilsTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testToSnakeCase1() {
+		String answer = StringUtils.toSnakeCase("aaa");
+		assertEquals("aaa", answer, "違うよ");
+		// fail("Not yet implemented");
+	}
+
+	@Test
+	void testToSnakeCase2() {
+		String answer = StringUtils.toSnakeCase("HelloWorld");
+		assertEquals("hello_world", answer, "違うよ");
+		// fail("Not yet implemented");
+	}
+
+	@Test
+	void testToSnakeCase3() {
+		String answer = StringUtils.toSnakeCase("practiceJunit");
+		assertEquals("practice_junit", answer, "違うよ");
+		// fail("Not yet implemented");
+	}
+
+}
